a2025df0f69bd802b1904ed4fe04629bf5c5cb88
Ignacio Loyola <ignacio.loyola@biomedica.udec.cl>
2024-09-07 15:59:15 -0400

    error handler

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from plugins.utils import fetch_weather_data, transform_observations, load_data_to_snowflake
+from plugins.utils import fetch_weather_data, transform_observations, connect_to_postgres, load_data_to_postgres
 
 def main():
     # Extract
@@ -8,8 +8,11 @@ def main():
     # Transform
     transformed_data = transform_observations(station_id, name_map, timezone_map, observations)
 
+    # Connect to db
+    connect_to_postgres()
+    
     # Load
-    load_data_to_snowflake(transformed_data)
+    load_data_to_postgres(transformed_data)
 
 
 if __name__ == "__main__":
